c6188b72fb019506ec52d7ac96ccc72418853906
umizuki-dev <umizuki@proton.me>
2026-02-17 16:56:49 +0000

    fixed scoping issues with turn enum

--- main.cc
+++ main.cc
@@ -1,22 +1,22 @@
 #include <iostream>
 
+enum Turn {
+  Sente,
+  Gote,
+};
+
 class Board {
   public:
-    int board[9][9];
+    int bitBoard[9][9];
     int komadaiSente[7];
     int komadaiGote [7];
-
-    enum Turn {
-      Sente,
-      Gote,
-    };
 
     Turn turn;
 
     Board() {
       for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-          board[i][j] = 0;
+          bitBoard[i][j] = 0;
         }
       }
 
@@ -24,50 +24,54 @@ class Board {
         komadaiSente[i] = 0;
         komadaiGote[i] = 0;
       }
-      
+
       turn = Sente;
     }
 
-    void print() {
-      /* pretty print turn */
-      std::cout << turn << "\n";
-
-      std::cout << "\n";
-
-      for (int i = 0; i < 7; i++) {
-        /* TODO: assign ints to peices */
-        /* TODO: pretty print komadai */
-        std::cout << komadaiSente[i];
-      }
-
-      /* space between komadai and board */
-      std::cout << "\n";
-      std::cout << "\n";
-
-      for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-          /* TODO: assign ints to peices */
-          std::cout << board[i][j];
-        }
-        std::cout << "\n";
-      }
-
-      /* space between komadai and board */
-      std::cout << "\n";
-
-      for (int i = 0; i < 7; i++) {
-        /* TODO: assign ints to peices */
-        /* TODO: pretty print komadai */
-        std::cout << komadaiGote[i];
-      }
-
-      /* space from bottom */
-      std::cout << "\n";
-    }
 };
+
+void printBoard(Board myBoard) {
+  std::cout << "手番：";
+  if (myBoard.turn == Sente) {
+    std::cout << "先手";
+  } else if (myBoard.turn == Gote) {
+    std::cout << "後手";
+  }
+  std::cout << "\n";
+
+  for (int i = 0; i < 7; i++) {
+    /* TODO: assign ints to peices */
+    /* TODO: pretty print komadai */
+    std::cout << myBoard.komadaiSente[i];
+  }
+
+  /* need space between komadai and board */
+  std::cout << "\n";
+  std::cout << "\n";
+
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      /* TODO: assign ints to peices */
+      std::cout << myBoard.bitBoard[i][j];
+    }
+    std::cout << "\n";
+  }
+
+  /* need space between komadai and board */
+  std::cout << "\n";
+
+  for (int i = 0; i < 7; i++) {
+    /* TODO: assign ints to peices */
+    /* TODO: pretty print komadai */
+    std::cout << myBoard.komadaiGote[i];
+  }
+
+  /* need to stop board clashing with prompt on exit */
+  std::cout << "\n";
+}
 
 int main() {
   Board gameBoard;
-  gameBoard.print();
+  printBoard(gameBoard);
   return 0;
 }
